938e5fd4733e8525046b57235709a3b2720abf9e
Devid <dovudsho.khudoydodov.97@gmail.com>
2020-08-28 21:39:48 +0500

    bugfix: tests work

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { WeatherService } from './weather.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WeatherService', () => {
-  let service: WeatherService;
+    let service: WeatherService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(WeatherService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule]
+        });
+        service = TestBed.inject(WeatherService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
